53253ebd469263093934e85e6274e958a56f612f
Dougal Sutherland <dougal@gmail.com>
2014-06-10 18:35:46 -0400

    add preprocessing transformers

---
+++ skl_groups/preprocessing.py
@@ -0,0 +1,227 @@
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.decomposition import PCA, RandomizedPCA
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
+
+from .features import Features
+
+# TODO: support inplace transformations here
+
+class BagPreprocesser(BaseEstimator, TransformerMixin):
+    '''
+    Applies a preprocessing estimator to each column of the bags, independently.
+    This is a reasonable thing to do for some cases
+    (especially :class:`sklearn.preprocessing.StandardScaler`,
+     :class:`sklearn.preprocessing.MinMaxScaler`,
+     :class:`sklearn.decomposition.PCA`).
+    It's not a reasonable thing to do for things that rely on interactions
+    between points, or that change the number of output points. (Changing
+    the dimension is okay.)
+
+    Parameters
+    ----------
+    transformer : an sklearn transformer
+        The transformer to apply to the stacked features. Must return the
+        same number of features.
+    '''
+    def __init__(self, transformer):
+        t = transformer
+        if (not (hasattr(t, "fit") or hasattr(t, 'fit_transform')) or
+                not hasattr(t, "transform")):
+            raise TypeError("The transformer doesn't have appropriate methods.")
+        self.transformer = t
+
+    def _check_inputs(self, X):
+        if isinstance(X, Features):
+            X.make_stacked()
+        else:
+            X = Features(X, stack=True)
+        return X
+
+    def _gather_outputs(self, old, new):
+        if new.shape[0] != old.total_points:
+            msg = "Transformer changed number of points from {} to {}"
+            raise ValueError(msg.format(old.total_points, new.shape[0]))
+        return Features(new, old.n_pts, **old.meta)
+
+    def fit(self, X, y=None, **params):
+        '''
+        Fit the transformer on the stacked points.
+
+        Parameters
+        ----------
+        X : :class:`Features` or list of arrays of shape ``[n_samples[i], n_features]``
+            Training set. If a Features object, it will be stacked.
+
+        any other keyword argument :
+            Passed on as keyword arguments to the transformer's ``fit()``.
+        '''
+        X = self._check_inputs(X)
+        self.transformer.fit(X.stacked_features, y, **params)
+        return self
+
+    def transform(self, X, **params):
+        '''
+        Transform the stacked points.
+
+        Parameters
+        ----------
+        X : :class:`Features` or list of bag feature arrays
+            New data to transform.
+
+        any other keyword argument :
+            Passed on as keyword arguments to the transformer's ``transform()``.
+
+        Returns
+        -------
+        X_new : :class:`Features`
+            Transformed features.
+        '''
+        X = self._check_inputs(X)
+        X_new = self.transformer.transform(X.stacked_features, **params)
+        return self._gather_outputs(X, X_new)
+
+    def fit_transform(self, X, y=None, **params):
+        '''
+        Fit and transform the stacked points.
+
+        Parameters
+        ----------
+        X : :class:`Features` or list of bag feature arrays
+            Data to train on and transform.
+
+        any other keyword argument :
+            Passed on as keyword arguments to the transformer's ``transform()``.
+
+        Returns
+        -------
+        X_new : :class:`Features`
+            Transformed features.
+        '''
+        X = self._check_inputs(X)
+        X_new = self.transformer.fit_transform(X.stacked_features, y, **params)
+        return self._gather_outputs(X, X_new)
+
+    def inverse_transform(self, X, **params):
+        '''
+        Transform data back to its original space, i.e., return an input
+        X_original whose transform would (maybe approximately) be X.
+
+        Parameters
+        ----------
+        X : :class:`Features` or list of bag feature arrays
+            Data to train on and transform.
+
+        any other keyword argument :
+            Passed on as keyword arguments to the transformer's 
+            ``inverse_transform()``.
+
+        Returns
+        -------
+        X_original : :class:`Features`
+        '''
+        X = self._check_inputs(X)
+        Xo = self.transformer.inverse_transform(X.stacked_features, **params)
+        return self._gather_outputs(X, Xo)
+
+
+class BagStandardizer(BagPreprocesser):
+    '''
+    Standardizes each feature dimension to have zero mean and unit variance,
+    regardless of the bag it falls into.
+
+    This is just :class:`BagPreprocesser` with
+    :class:`sklearn.preprocessing.StandardScaler`.
+    '''
+    def __init__(self):
+        super(BagStandardizer, self).__init__(StandardScaler())
+
+
+class BagMinMaxScaler(BagPreprocesser):
+    '''
+    Linearly scales each feature dimension to lie within the given range, e.g.
+    [0, 1].
+
+    This is just :class:`BagPreprocesser` with
+    :class:`sklearn.preprocessing.MinMaxScaler`.
+
+    Parameters
+    ----------
+    feature_range : tuple (min, max), default = (0, 1)
+        Desired range of the transformed data.
+    '''
+    def __init__(self, feature_range=(0, 1)):
+        super(BagMinMaxScaler, self).__init__(MinMaxScaler(feature_range))
+
+
+class BagNormalizer(BagPreprocesser):
+    '''
+    Normalizes each sample individually to have unit norm (l1 or l2).
+
+    This is just :class:`BagPreprocesser` with
+    :class:`sklearn.preprocessing.Normalizer`.
+
+    Parameters
+    ----------
+    norm : 'l1' or 'l2', optional, default 'l2'
+        The norm to use to normalize each nonzero sample.
+    '''
+    def __init__(self, norm='l2'):
+        super(BagNormalizer, self).__init__(Normalizer(norm))
+
+
+DEFAULT_VARFRAC = 0.7
+class BagPCA(BagPreprocesser):
+    '''
+    Runs principal components analysis to reduce the dimensionality of the
+    features.
+
+    This is just :class:`BagPreprocesser` with
+    either :class:`sklearn.decomposition.PCA`
+    or :class:`sklearn.decomposition.RandomizedPCA`.
+
+    Parameters
+    ----------
+    k : int, optional
+        The dimensionality to reduce to.
+
+    mle_components : boolean, optional, default False
+        Use Minka's MLE for determining the number of components.
+
+    varfrac : float in (0, 1], optional, default 0.7
+        Use enough components to cover this fraction of the variance.
+        Only one of {k, mle_components, varfrac} can be passed.
+
+    randomize : boolean, optional, default False
+        Use a randomized PCA. This can be faster and less memory-intensive for
+        large inputs, but is approximate and requires specifying an explicit
+        number of components.
+
+    whiten : boolean, optional, default False
+        Whether to whiten the outputs, by dividing the components by the
+        singular values. This removes some information, but makes the variance
+        of the outputs the identity matrix.
+    '''
+    def __init__(self, k=None, mle_components=False, varfrac=None,
+                 randomize=False, whiten=False):
+        n_specs = sum(1 for x in [k, mle_components, varfrac] if x)
+        if n_specs > 1:
+            msg = "can't specify number of components in more than one way"
+            raise TypeError(msg)
+        if n_specs == 0:
+            varfrac = DEFAULT_VARFRAC
+
+        if randomize:
+            if k is None:
+                raise TypeError("can't do random PCA without a specific k")
+            pca = RandomizedPCA(k, whiten=whiten)
+        else:
+            if k is not None:
+                n_components = k
+            elif mle_components:
+                n_components = 'mle'
+            elif varfrac is not None:
+                n_components = varfrac
+            pca = PCA(n_components, whiten=whiten)
+        super(BagPCA, self).__init__(pca)
---
+++ skl_groups/tests/test_preprocessing.py
@@ -0,0 +1,89 @@
+from __future__ import division
+
+import os
+import sys
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.externals.six.moves import xrange
+from nose.tools import assert_raises
+
+if __name__ == '__main__':
+    # make this copy of skl_groups importable
+    _this_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(_this_dir)))
+
+from skl_groups import Features
+from skl_groups.preprocessing import (BagPreprocesser, BagStandardizer,
+                                      BagMinMaxScaler, BagNormalizer, BagPCA)
+
+
+################################################################################
+
+def test_basic():
+    bags = [np.random.normal(5, 3, size=(np.random.randint(10, 100), 20))
+            for _ in xrange(50)]
+    feats = Features(bags, stack=True)
+
+    stder = BagStandardizer()
+    stdized = stder.fit_transform(bags)
+    stdized.make_stacked()
+
+    assert np.allclose(np.mean(stdized.stacked_features), 0)
+    assert np.allclose(np.std(stdized.stacked_features), 1)
+
+    first_five = stder.transform(bags[:5])
+    assert first_five == stdized[:5]
+
+    minmaxer = BagMinMaxScaler([3, 7])
+    minmaxed = minmaxer.fit_transform(bags)
+    minmaxed.make_stacked()
+    assert np.allclose(np.min(minmaxed.stacked_features, 0), 3)
+    assert np.allclose(np.max(minmaxed.stacked_features, 0), 7)
+
+    normer = BagNormalizer('l1')
+    normed = normer.fit_transform(Features(bags))
+    normed.make_stacked()
+    assert np.allclose(np.sum(np.abs(normed.stacked_features), 1), 1)
+
+    class GetMean(BaseEstimator, TransformerMixin):
+        def fit(self, X, y=None):
+            return self
+        def transform(self, X):
+            return X.mean(axis=1)[None, :]
+    m = BagPreprocesser(GetMean())
+    assert_raises(ValueError, lambda: m.transform(bags))
+
+
+def test_pca():
+    bags = [np.random.normal(5, 3, size=(np.random.randint(10, 100), 20))
+            for _ in xrange(50)]
+    feats = Features(bags, stack=True)
+
+    pca = BagPCA(k=3)
+    pca.fit(bags)
+    pcaed = pca.transform(bags)
+    assert pcaed.dim == 3
+
+    BagPCA(varfrac=.3).fit_transform(bags)
+
+    pca2 = BagPCA(k=20)
+    pcaed2 = pca2.fit_transform(bags)
+    orig = pca2.inverse_transform(pcaed2)
+    orig.make_stacked()
+    print np.abs(feats.stacked_features - orig.stacked_features).mean()
+    assert np.allclose(feats.stacked_features, orig.stacked_features)
+
+    assert BagPCA(k=5, randomize=True).fit_transform(bags).dim == 5
+
+    assert_raises(TypeError, lambda: BagPCA(randomize=True))
+    assert_raises(TypeError, lambda: BagPCA(mle_components=True, k=12))
+    assert BagPCA(mle_components=True)
+
+
+
+################################################################################
+
+if __name__ == '__main__':
+    import nose
+    nose.main()
