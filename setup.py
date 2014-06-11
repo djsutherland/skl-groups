95942859c80a1b19ba535806b91670c9f12dfbef
Dougal Sutherland <dougal@gmail.com>
2014-06-11 11:18:27 -0400

    add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from distutils.extension import Extension
+
+DESCRIPTION = "Addon to scikit-learn for handling set-based data."
+with open('README.rst') as f:
+    LONG_DESCRIPTION = f.read()
+NAME = "skl-groups"
+AUTHOR = MAINTAINER = "Dougal J. Sutherland"
+AUTHOR_EMAIL = MAINTAINER_EMAIL = "dsutherl@cs.cmu.edu"
+URL = DOWNLOAD_URL = "https://github.com/dougalsutherland/skl-groups"
+LICENSE = "BSD"
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "Topic :: Scientific/Engineering :: Image Recognition",
+    "Topic :: Scientific/Engineering :: Information Analysis",
+]
+
+import skl_groups
+VERSION = skl_groups.__version__
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'scikit-learn >= 0.13',
+    ],
+    zip_safe=False,
+)
--- skl_groups/__init__.py
+++ skl_groups/__init__.py
@@ -1,1 +1,3 @@
 from .features import Features
+
+__version__ = '0.1.0-dev'
