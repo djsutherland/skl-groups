a35d7291544395062b8163570f918b4eeca05774
Dougal J. Sutherland <dougal@gmail.com>
2014-08-23 20:17:43 -0400

    fix travis test script a bit more

--- continuous_integration/test.sh
+++ continuous_integration/test.sh
@@ -2,12 +2,14 @@
 # the Travis "script" step: see http://docs.travis-ci.com/ and ../.travis.yml
 set -e
 
+cd $HOME # get out of source directory to avoid confusing nose
+
 source activate without-flann
-nosetests --exe skl-groups
+nosetests --exe skl_groups
 source deactivate
 
 for env in with-pyflann with-cyflann with-accel; do
     source activate $env
-    nosetests --exe skl-groups.tests.test_divs_knn
+    nosetests --exe skl_groups.tests.test_divs_knn
     source deactivate
 done
