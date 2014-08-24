4bde0fdc7b1e28807adaea8cc8ff9e855cb27768
Dougal J. Sutherland <dougal@gmail.com>
2014-08-23 20:01:41 -0400

    give Travis a shot

---
+++ continuous_integration/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# the Travis "install" step: see http://docs.travis-ci.com/ and ../.travis.yml
+set -e
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+        -O miniconda.sh
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-MacOSX-x86_64.sh \
+        -O miniconda.sh
+else
+    echo "unknown os $TRAVIS_OS_NAME"
+    exit 1
+fi
+chmod +x miniconda.sh
+./miniconda.sh -b
+export PATH="$HOME/miniconda/bin:$PATH"
+
+conda update --yes --quiet conda
+
+PKGS="python=$PYTHON_VERSION pip nose setuptools testfixtures cython"
+PKGS="$PKGS numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION"
+PKGS="$PKGS scikit-learn=$SKLEARN_VERSION"
+
+conda create --yes -n without-flann -c https://conda.binstar.org/dougal \
+    $PKGS
+
+conda create --yes -n with-pyflann -c https://conda.binstar.org/dougal \
+    $PKGS pyflann=$PYFLANN_VERSION
+
+conda create --yes -n with-cyflann -c https://conda.binstar.org/dougal \
+    $PKGS cyflann=$CYFLANN_VERSION
+
+conda create --yes -n with-accel -c https://conda.binstar.org/dougal \
+    $PKGS cyflann=$CYFLANN_VERSION
+
+
+source activate without-flann
+python setup.py install
+source deactivate
+
+source activate with-pyflann
+python setup.py install
+source deactivate
+
+source activate with-cyflann
+python setup.py install
+source deactivate
+
+source activate with-accel
+python setup.py install
+python setup_accel.py install
+source deactivate
---
+++ continuous_integration/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# the Travis "script" step: see http://docs.travis-ci.com/ and ../.travis.yml
+set -e
+
+source activate without-flann
+pass=nosetests --exe skl-groups
+source deactivate
+
+for env in with-pyflann with-cyflann with-accel; do
+    source activate $env
+    nosetests --exe skl-groups.tests.test_divs_knn
+    source deactivate
+done
