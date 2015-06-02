bc4b089eba4f611848a33c3b97909bdcf2613eee
Dougal J. Sutherland <dougal@gmail.com>
2015-06-02 03:30:07 -0400

    fix a silly mistake in make_stacked  :|

--- skl_groups/features.py
+++ skl_groups/features.py
@@ -223,8 +223,9 @@ class Features(object):
 
         self._boundaries = bounds = np.r_[0, np.cumsum(self.n_pts)]
         self.stacked_features = stacked = np.vstack(self.features)
-        self.features = [stacked[bounds[i-1]:bounds[i]]
-                         for i in xrange(1, len(bounds))]
+        self.features = np.array(
+            [stacked[bounds[i-1]:bounds[i]] for i in xrange(1, len(bounds))],
+            dtype=object)
         self.stacked = True
 
     ############################################################################
